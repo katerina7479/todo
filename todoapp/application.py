f924e61bb8824ed78d83e4cf109f14ca874bf554
Katerina Hanson <katerina.hanson@gmail.com>
2014-07-03 17:52:56 -0700

    Found sqlite boolean

--- todoapp/application.py
+++ todoapp/application.py
@@ -21,7 +21,7 @@ def todo(todo_id):
     if request.method == "POST":
         title = request.form["todo"]
         notes = request.form["notes"]
-        done = 0 if str(request.form["done"]) == 'false' else 1  # Sqlite need booleans tp be ints
+        done = False if str(request.form["done"]) == 'false' else True  # JS to python false
         if todo_id is not None: # Existing, get it
             todo = db_session.query(Todo).get(todo_id)
             todo.title = title
@@ -29,7 +29,7 @@ def todo(todo_id):
             todo.done = done
         else: # It's new
             todo = Todo(title=title, description=notes, done=done)
-        db_session.add(todo)
+            db_session.add(todo)
         db_session.commit()
         return "OK"
     else: # GET
--- todoapp/models.py
+++ todoapp/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Table, Column, Integer, String, Boolean
+from sqlalchemy import Table, Column, Integer, String
 from sqlalchemy.orm import mapper
+from sqlalchemy.dialects.sqlite.base import BOOLEAN
 from database import metadata, db_session
 
 class Todo(object):
@@ -17,7 +18,7 @@ todo_table = Table('todo', metadata,
                    Column('id', Integer, primary_key=True),
                    Column('title', String(50), unique=True),
                    Column('description', String(120)),
-                   Column('done', Boolean)
+                   Column('done', BOOLEAN)
                    )
 
 mapper(Todo, todo_table)
